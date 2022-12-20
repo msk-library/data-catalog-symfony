520477863ba96fecb91f316acf1b4167a98d4405
Eric Willoughby <willouge@mskcc.org>
2022-12-20 10:41:58 -0500

    Deprecate Method from Sensio, replaced by Route

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\Type\DatasetViaApiType;
--- src/Controller/AddController.php
+++ src/Controller/AddController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Request;
