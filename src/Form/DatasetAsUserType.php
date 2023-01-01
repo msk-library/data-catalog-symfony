e48488e81750a6cd94c4c31fbcab5ad90896a414
Eric Willoughby <willouge@mskcc.org>
2023-01-01 14:01:59 -0500

    Use full classpath for Entity class - resolves issue and avoids including classes in use statement

--- src/Form/DatasetAsUserType.php
+++ src/Form/DatasetAsUserType.php
@@ -71,7 +71,7 @@ class DatasetAsUserType extends AbstractType {
       'label'    => 'Access Instructions'));
     //technical details
     $builder->add('related_equipment', 'entity', array(
-      'class'   => 'App:RelatedEquipment',
+      'class'   => 'App\Entity\RelatedEquipment',
       'property'=> 'related_equipment',
       'query_builder'=> function(EntityRepository $er) {
           return $er->createQueryBuilder('u')->orderBy('u.related_equipment','ASC');
@@ -83,7 +83,7 @@ class DatasetAsUserType extends AbstractType {
       'label'     => 'Equipment used to collect/create the dataset',
     ));
     $builder->add('related_software', 'entity', array(
-      'class'   => 'App:RelatedSoftware',
+      'class'   => 'App\Entity\RelatedSoftware',
       'property'=> 'software_name',
       'query_builder'=> function(EntityRepository $er) {
           return $er->createQueryBuilder('u')->orderBy('u.software_name','ASC');
@@ -95,7 +95,7 @@ class DatasetAsUserType extends AbstractType {
       'label'     => 'Software used to create, collect or analyze the dataset',
     ));
     $builder->add('dataset_formats', 'entity', array(
-      'class'   => 'App:DatasetFormat',
+      'class'   => 'App\Entity\DatasetFormat',
       'property'=> 'format',
       'query_builder'=> function(EntityRepository $er) {
           return $er->createQueryBuilder('u')->orderBy('u.format','ASC');
@@ -107,7 +107,7 @@ class DatasetAsUserType extends AbstractType {
       'label'     => 'Dataset File Format',
     ));
     $builder->add('data_collection_instruments', 'entity', array(
-      'class'   => 'App:DataCollectionInstrument',
+      'class'   => 'App\Entity\DataCollectionInstrument',
       'property'=> 'data_collection_instrument_name',
       'required' => false,
       'attr'=>array('style'=>'width:100%', 'placeholder'=>''),
@@ -134,7 +134,7 @@ class DatasetAsUserType extends AbstractType {
       'required' => false,
       'label'    => 'Year Data Collection Ended'));
     $builder->add('subject_of_study', 'entity', array(
-      'class'    => 'App:SubjectOfStudy',
+      'class'    => 'App\Entity\SubjectOfStudy',
       'property' => 'subject_of_study',
       'required' => false,
       'query_builder'=> function(EntityRepository $er) {
@@ -146,7 +146,7 @@ class DatasetAsUserType extends AbstractType {
       'label'     => 'Subject of Study',
     ));
     $builder->add('subject_keywords', 'entity', array(
-      'class'   => 'App:SubjectKeyword',
+      'class'   => 'App\Entity\SubjectKeyword',
       'property'=> 'keyword',
       'required' => false,
       'query_builder'=> function(EntityRepository $er) {
@@ -158,7 +158,7 @@ class DatasetAsUserType extends AbstractType {
       'label'     => 'Subject Keywords',
     ));
     $builder->add('onco_trees', EntityType::class, array(
-      'class'   => 'App:OncoTree',
+      'class'   => 'App\Entity\OncoTree',
       'choice_label'=> 'onco_tree_code',
       'required' => false,
       'query_builder'=> function(EntityRepository $er) {
