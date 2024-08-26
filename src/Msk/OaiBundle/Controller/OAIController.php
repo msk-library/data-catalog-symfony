30bf67d505614c22bafc1d247dac5aaefbf45e0f
Eric Willoughby <willouge@mskcc.org>
2024-08-26 11:45:19 -0400

    Add syntax changes, update routes to attributes, add oai namespace to templates for bundle

--- src/Msk/OaiBundle/Controller/OAIController.php
+++ src/Msk/OaiBundle/Controller/OAIController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Msk\OaiBundle\Controller;
-
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Dataset;
+
 
 
 class OAIController extends AbstractController
@@ -20,10 +20,10 @@ class OAIController extends AbstractController
    *
    * @return Response A Response instance
    *
-   * @Route("/oai/", name="oai_base")
    * 
    */
-    public function indexAction(Request $request, EntityManagerInterface $em){
+  #[Route(path: '/oai', name: 'oai_base')]
+    public function index(Request $request, EntityManagerInterface $em) : Response {
         date_default_timezone_set('UTC');
 
         $base_url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'];
@@ -32,11 +32,11 @@ class OAIController extends AbstractController
         if(isset($_GET['identifier'])){
             $dataset_uid = explode(':', $_GET['identifier']);
             $dataset_uid = (int)end($dataset_uid);
-            $results = $em->getRepository('Dataset::Class')->findBy(['dataset_uid'=>$dataset_uid]);
+            $results = $em->getRepository(Dataset::Class)->findBy(['dataset_uid'=>$dataset_uid]);
         }
 
         else{
-            $results = $em->getRepository('Dataset::Class')->findAll();
+            $results = $em->getRepository(Dataset::Class)->findAll();
         }
         //dump($results);
 
@@ -44,26 +44,26 @@ class OAIController extends AbstractController
         $verb = isset($_GET['verb'])?trim($_GET['verb']):'Identify';
         switch($verb){
             case 'Identify':
-                $template = 'oai_identify.xml.twig';
+                $template = '@oai/oai_identify.xml.twig';
                 break;
             case 'ListRecords':
-                $template = 'oai_list_records.xml.twig';
+                $template = '@oai/oai_list_records.xml.twig';
                 break;
             case 'GetRecord':
-                $template = 'oai_list_records.xml.twig';
+                $template = '@oai/oai_list_records.xml.twig';
                 break;
             case 'ListSets':
-                $template = 'oai_list_sets.xml.twig';
+                $template = '@oai/oai_list_sets.xml.twig';
                 break;
             case 'ListMetadataFormats':
-                    $template = 'oai_list_metadata_formats.xml.twig';
+                    $template = '@oai/oai_list_metadata_formats.xml.twig';
                     break;
             case 'ListIdentifiers':
-                $template = 'oai_list_identifiers.xml.twig';
+                $template = '@oai/oai_list_identifiers.xml.twig';
                 break;
         }
         $response = new Response(
-            $this->renderView('oai_base.xml.twig', array(
+            $this->renderView('@oai/oai_base.xml.twig', array(
                 'oai_template' => $template,
                 'timestamp' => date('Y-m-d\TH:i:s\Z'),
                 'base_url' => $base_url,
